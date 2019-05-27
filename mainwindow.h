6509f786b9f3250ccd2f049df8267e00ec4ddb1c
sehuanes <47614953+sehuanes@users.noreply.github.com>
2019-05-27 15:09:51 -0500

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void digit_pressed();
+    void on_punto_released();
+    void unary_operation_pressed();
+    void on_clear_released();
+    void on_igual_released();
+    void binario_operation_pressed();
+};
+
+#endif // MAINWINDOW_H
